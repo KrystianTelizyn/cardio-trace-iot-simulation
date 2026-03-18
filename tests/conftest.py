0af840ae2773b0c6aa2be63be51ab7325919a745
Krystian Telizyn <krystian.telizyn@gmail.com>
2026-03-18 14:06:45 +0100

    feat(simulator): MQTT integration (#9)
    * feat: introducing adapter for aiomqtt client to be compatible with simulator
    
    * test: Mqtt integration tests
    
    * test: updates to test fixtures to make correct test flow for test_simulator_config.py
    
    ---------
    
    Co-authored-by: KrystianTelizyn <krysian.telizyn@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import pytest
 import json
 from hr_monitor.formats import PayloadTemplates
 from hr_monitor.simulator import HRDeviceConfig
+from hr_monitor.protocols import MqttClient
+from hr_monitor.simulator import HRSimulatorConfig, HRMonitorMqttSimulator
 
 
 @pytest.fixture
@@ -47,7 +49,7 @@ def expected_devices_list():
     return [
         HRDeviceConfig(
             device_id="runner",
-            payload_format="PayloadTemplates.Apple",
+            payload_format="<device_id> <frame>",
             topic="example/runner",
             hr_frame=2,
             hrv_frame=9,
@@ -55,7 +57,7 @@ def expected_devices_list():
         ),
         HRDeviceConfig(
             device_id="walker",
-            payload_format="PayloadTemplates.Garmin",
+            payload_format="<device_id> <frame>",
             topic="example/walker",
             hr_frame=2,
             hrv_frame=9,
@@ -67,3 +69,31 @@ def expected_devices_list():
 @pytest.fixture
 def example_config_file():
     return "tests/example_config.json"
+
+
+@pytest.fixture
+def mock_repository(mocker):
+    mock_repository = mocker.Mock()
+    mock_repository.get_record_data.return_value = [1, 2, 3, 4, 5]
+    return mock_repository
+
+
+@pytest.fixture
+def mock_mqtt_client(mocker):
+    mock_mqtt_client = mocker.AsyncMock(spec=MqttClient)
+    return mock_mqtt_client
+
+
+@pytest.fixture
+async def simulator_mqtt_mock(
+    mocker, example_config_file, mock_repository, mock_mqtt_client
+):
+    sim = HRMonitorMqttSimulator(
+        repository=mock_repository,
+        config=HRSimulatorConfig.from_json_file(example_config_file),
+        mqtt_client=mock_mqtt_client,
+    )
+    try:
+        yield sim
+    finally:
+        await sim.close()
---
+++ tests/test_mqtt_integration.py
@@ -0,0 +1,97 @@
+from testcontainers.core.container import DockerContainer
+from aiomqtt import Client
+import pytest
+from itertools import chain
+from hr_monitor import HRMonitorMqttSimulator, HRSimulatorConfig
+from hr_monitor.adapters import AioMqttClientAdapter
+
+
+@pytest.fixture(scope="module")
+def mosquitto_broker():
+    with DockerContainer("eclipse-mosquitto:latest").with_exposed_ports(
+        1883
+    ) as container:
+        host = container.get_container_host_ip()
+        port = int(container.get_exposed_port(1883))
+        yield host, port
+
+
+@pytest.fixture
+async def mqtt_client_adapter(mosquitto_broker):
+    host, port = mosquitto_broker
+    client = AioMqttClientAdapter(hostname=host, port=port)
+    yield client
+    await client.disconnect()
+
+
+@pytest.fixture
+async def mqtt_client(mosquitto_broker):
+    host, port = mosquitto_broker
+    async with Client(hostname=host, port=port, timeout=20) as client:
+        yield client
+
+
+@pytest.fixture
+async def simulator_with_mqtt(
+    mqtt_client_adapter, example_config_file, mock_repository
+):
+    simulator = HRMonitorMqttSimulator(
+        config=HRSimulatorConfig.from_json_file(example_config_file),
+        mqtt_client=mqtt_client_adapter,
+        repository=mock_repository,
+    )
+    try:
+        yield simulator
+    finally:
+        await simulator.close()
+
+
+@pytest.mark.timeout(10)
+@pytest.mark.asyncio
+async def test_mqtt_integration_runner(simulator_with_mqtt, mqtt_client):
+    await mqtt_client.subscribe("example/runner")
+    await simulator_with_mqtt.start()
+    # obtain first 5 messages
+    messages = []
+    async for message in mqtt_client.messages:
+        messages.append(message)
+        if len(messages) >= 5:
+            break
+    await simulator_with_mqtt.stop()
+    for i, message in enumerate(messages):
+        assert message.payload.decode("utf-8") == f"runner {i + 1}"
+
+
+@pytest.mark.timeout(10)
+@pytest.mark.asyncio
+async def test_mqtt_integration_walker(simulator_with_mqtt, mqtt_client):
+    await mqtt_client.subscribe("example/walker")
+    await simulator_with_mqtt.start()
+    # obtain first 5 messages
+    messages = []
+    async for message in mqtt_client.messages:
+        messages.append(message)
+        if len(messages) >= 5:
+            break
+    await simulator_with_mqtt.stop()
+    for i, message in enumerate(messages):
+        assert message.payload.decode("utf-8") == f"walker {i + 1}"
+
+
+@pytest.mark.timeout(10)
+@pytest.mark.asyncio
+async def test_mqtt_integration_walker_runner(simulator_with_mqtt, mqtt_client):
+    await mqtt_client.subscribe("example/#")
+    await simulator_with_mqtt.start()
+    # obtain first 10 messages
+    messages = {"example/runner": [], "example/walker": []}
+    async for message in mqtt_client.messages:
+        messages[message.topic.value].append(message)
+        # check if we have 10 messages
+        if len(list(chain(*messages.values()))) >= 10:
+            break
+    await simulator_with_mqtt.stop()
+    for i, message in enumerate(messages["example/walker"]):
+        assert message.payload.decode("utf-8") == f"walker {i + 1}"
+    for i, message in enumerate(messages["example/runner"]):
+        assert message.payload.decode("utf-8") == f"runner {i + 1}"
